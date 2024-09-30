63bb25e643c9e0fc87c198a45f029a7445730a34
facundoprocelli <128741375+facundoprocelli@users.noreply.github.com>
2024-09-30 18:15:20 -0300

    Clase persona e interfaz
    Empezamos con la GUI para los casos en los que hay wifi
    Creamos la clase persona para poder manejar los mensajes que nos envíen desde DEPORTNET
    
    Co-Authored-By: Fabrizio Golisciano <72052696+Fabri-dev@users.noreply.github.com>

--- AplicacionReconocimiento/GUI/WFPrincipal.cs
+++ AplicacionReconocimiento/GUI/WFPrincipal.cs
@@ -1,12 +1,18 @@
-﻿using System;
+﻿using DeportNetReconocimiento.Modelo;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO.Pipes;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
+using System.Text.Json.Nodes;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
+
 
 namespace DeportNetReconocimiento.GUI
 {
@@ -15,7 +21,127 @@ namespace DeportNetReconocimiento.GUI
         public WFPrincipal()
         {
             InitializeComponent();
-            
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void WFPrincipal_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void apellido_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void nombreText_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void actividadText_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void clasesRestaintesText_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void mensajeText_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void clasesRestantesText(object sender, EventArgs e)
+        {
+
+        }
+
+        public void actualizarDatos(string json)
+        {
+
+            Persona persona = JSONtoPersona(json);
+
+
+            apellidoText.Text = persona.Apellido;
+            nombreText.Text = persona.Nombre;
+            actividadText.Text = persona.Actividad;
+            clasesRestaintesText.Text = persona.ClasesRestantes;
+            mensajeText.Text = persona.Mensaje;
+
+
+
+        }
+
+
+        public static Persona JSONtoPersona(string json)
+        {
+
+            Persona persona = new Persona();
+
+            using (JsonDocument doc = JsonDocument.Parse(json))
+            {
+                JsonElement root = doc.RootElement;
+
+                // Acceder a cada campo del objeto JSON
+                persona.Id = root.GetProperty("Id").GetInt32();
+                persona.Apellido = root.GetProperty("Apellido").GetString();
+                persona.Nombre = root.GetProperty("Nombre").GetString();
+                persona.Actividad = root.GetProperty("Actividad").GetString();
+                persona.ClasesRestantes = root.GetProperty("ClasesRestantes").GetString();
+                persona.Mensaje = root.GetProperty("Mensaje").GetString();
+
+            }
+            return persona;
+
+
         }
     }
 }
---
+++ AplicacionReconocimiento/Modelo/Persona.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeportNetReconocimiento.Modelo
+{
+    public class Persona
+    {
+
+       private  int id { get; set; }
+        private string nombre { get; set; }
+        private string apellido { get; set; }
+        private string actividad { get; set; }
+        private string clasesRestantes { get; set; }
+        private string mensaje { get; set; }
+
+        public Persona(int id, string nombre, string apellido, string actividad, string clasesRestantes, string mensaje) { 
+        
+            this.id = id;
+            this.nombre = nombre;   
+            this.apellido = apellido;   
+            this.actividad = actividad;
+            this.clasesRestantes = clasesRestantes;
+            this.mensaje = mensaje;        
+        }
+
+        public Persona() { }
+
+        // Propiedades con getter y setter
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        public string Apellido
+        {
+            get { return apellido; }
+            set { apellido = value; }
+        }
+
+        public string Actividad
+        {
+            get { return actividad; }
+            set { actividad = value; }
+        }
+
+        public string ClasesRestantes
+        {
+            get { return clasesRestantes; }
+            set { clasesRestantes = value; }
+        }
+
+        public string Mensaje
+        {
+            get { return mensaje; }
+            set { mensaje = value; }
+        }
+
+    }
+}
