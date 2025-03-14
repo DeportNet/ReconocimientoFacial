3b39f245aa525a246e35176229d7729df56c6c79
Fabri-dev <fabri.vogol@gmail.com>
2025-03-14 11:23:37 -0300

    cambios

--- AplicacionReconocimiento/obj/Release/net8.0-windows10.0.26100.0/DeportNetReconocimiento.AssemblyInfo.cs
+++ AplicacionReconocimiento/obj/Release/net8.0-windows10.0.26100.0/DeportNetReconocimiento.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyCopyrightAttribute("Deportnet")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0591d841daac0086497b09a0657dbe7908c8f543")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c160fcde4cc72f4770693fc56eecf71cd6d44c3a")]
 [assembly: System.Reflection.AssemblyProductAttribute("DeportNetReconocimiento")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DeportNetReconocimiento")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
