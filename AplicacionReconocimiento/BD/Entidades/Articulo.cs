0221ddcf15f636b4e26031e4601cee45a0852634
Fabri-dev <fabri.vogol@gmail.com>
2025-03-17 12:41:33 -0300

    agregamos entidades

--- AplicacionReconocimiento/BD/Entidades/Articulo.cs
+++ AplicacionReconocimiento/BD/Entidades/Articulo.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,22 @@ namespace DeportNetReconocimiento.Modelo
 {
     public class Articulo
     {
+        [Key]
+        [Column("id")]
+        private int id { get; set; }
+
+        [Column("id_dx")]
+        private int IdDx { get; set; }
+        private string Name { get; set; }
+        private double Amount { get; set; }
+        private string IsSaleItem { get; set; } // 'T' = Articulo, 'F' = Servicio
+
+        public Articulo(int idDx, string nombre, double precio, string esUnArticulo)
+        {
+            IdDx = idDx;
+            Name = nombre;
+            Amount = precio;
+            IsSaleItem = esUnArticulo;
+        }
     }
 }
--- AplicacionReconocimiento/BD/Entidades/ConfiguracionDeAcceso.cs
+++ AplicacionReconocimiento/BD/Entidades/ConfiguracionDeAcceso.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DeportNetReconocimiento.Modelo
 {
-    internal class ConfiguracionDeAcceso
+    [Table("configuracion_de_acceso")]
+    public class ConfiguracionDeAcceso
     {
+        private int CardLength { get; set; }
+        private string StartCharacter { get; set; }
+        private string EndCharacter { get; set; }
+        private string SecondStartCharacter { get; set; }
+
+        public ConfiguracionDeAcceso(int cardLength, string startCharacter, string endCharacter, string secondStartCharacter)
+        {
+            CardLength = cardLength;
+            StartCharacter = startCharacter;
+            EndCharacter = endCharacter;
+            SecondStartCharacter = secondStartCharacter;
+        }
     }
 }
