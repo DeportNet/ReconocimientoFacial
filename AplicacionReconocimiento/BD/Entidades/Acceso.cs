6e37dbb45ad94f52677aab7313879512bb8f5a08
Fabri-dev <fabri.vogol@gmail.com>
2025-03-17 12:48:18 -0300

    Update Acceso.cs

--- AplicacionReconocimiento/BD/Entidades/Acceso.cs
+++ AplicacionReconocimiento/BD/Entidades/Acceso.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using DeportNetReconocimiento.BD.Entidades;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +11,26 @@ namespace DeportNetReconocimiento.Modelo
 {
     public class Acceso
     {
+        [Key]
+        [Column("id")]
+        private int Id { get; set; }
+        private int ActiveBranchId { get; set; }
+        private int ProcessId { get; set; }
+        private List<AccesoMiembro> MemberAccess{get; set;}
+
+        public Acceso(int activeBranchId, int processId)
+        {
+            ActiveBranchId = activeBranchId;
+            ProcessId = processId;
+            MemberAccess = new List<AccesoMiembro>();
+        }
+
+        public Acceso(int activeBranchId, int processId, List<AccesoMiembro> memberAccess)
+        {
+            ActiveBranchId = activeBranchId;
+            ProcessId = processId;
+            MemberAccess = memberAccess;
+        }
+
     }
 }
