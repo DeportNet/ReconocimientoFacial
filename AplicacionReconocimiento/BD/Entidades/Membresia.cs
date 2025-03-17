92c614262f5c378c3d6d16ccda7cb29063a497ab
Fabri-dev <fabri.vogol@gmail.com>
2025-03-17 11:47:40 -0300

    creamos nuevo proyecto para el api

---
+++ AplicacionReconocimiento/BD/Entidades/Membresia.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeportNetReconocimiento.BD.Entidades
+{
+    [Table("membresias")]
+    public class Membresia
+    {
+        [Key]
+        [Column("id")]
+        public int Id { get; }
+        public int IdDx { get; set; }
+        public string Name { get; set; }
+        public double Amount { get; set; }
+        public string IsSaleItem { get; set; } // 'T' = Articulo, 'F' = Servicio 
+        public string Period { get; set; }
+        public string Days { get; set; }
+
+        public Membresia(int idDx, string nombre, double precio, string esUnArticulo, string periodo, string dias)
+        {
+            IdDx = idDx;
+            Name = nombre;
+            Amount = precio;
+            IsSaleItem = esUnArticulo;
+            Period = periodo;
+            Days = dias;
+        }
+
+    }
+}
--- AplicacionReconocimiento/BD/Entidades/Socio.cs
+++ AplicacionReconocimiento/BD/Entidades/Socio.cs
@@ -7,7 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Diagnostics.CodeAnalysis;
 
-namespace DeportNetReconocimiento.Modelo
+namespace DeportNetReconocimiento.BD.Entidades
 {
     [Table("socios")]
     public class Socio
@@ -32,61 +32,83 @@ namespace DeportNetReconocimiento.Modelo
         [StringLength(100)]
         [DisallowNull]
         [Required]
-        public required string Nombre { get; set; }
+        public required string FirstName { get; set; }
 
         [Required]
         [Column("lastName")]
         [StringLength(100)]
         [DisallowNull]
-        public required string Apellido { get; set; }
+        public required string LastName { get; set; }
 
         [Column("idNumber")]
         [StringLength(50)]
         [AllowNull]
-        public string? NumeroDocumento { get; set; }
+        public string? IdNumber { get; set; }
 
         [Column("birthDate")]
         [AllowNull]
-        public DateTime? FechaNacimiento { get; set; }
+        public DateTime? BirthDate { get; set; }
 
         [Column("cellphone")]
         [StringLength(50)]
         [AllowNull]
-        public string? Celular { get; set; }
+        public string? Cellphone { get; set; }
 
         [Column("isActive")]
         [StringLength(1)]
         [AllowNull]
-        public string? Activo { get; set; } // '1' = Activo, '0' = Inactivo
+        public string? IsActive { get; set; } // '1' = Activo, '0' = Inactivo
 
         [Column("cardNumber")]
         [StringLength(100)]
         [AllowNull]
-        public string? TarjetaIngreso { get; set; }
+        public string? CardNumber { get; set; }
 
         [Column("address")]
         [StringLength(100)]
         [AllowNull]
-        public string? Domicilio { get; set; }
+        public string? Address { get; set; }
 
         [Column("addressWithFloor")]
         [StringLength(100)]
         [AllowNull]
-        public string? DomicilioConPiso { get; set; }
+        public string? AddressWithFloor { get; set; }
 
         [Column("imageUrl")]
         [StringLength(250)]
         [AllowNull]
-        public string? ImagenUrl { get; set; }
+        public string? ImageUrl { get; set; }
 
         [Column("gender")]
         [StringLength(1)]
         [AllowNull]
-        public string? Sexo { get; set; } // 'f' = Femenino, 'm' = Masculino
+        public string? Gender { get; set; } // 'f' = Femenino, 'm' = Masculino
 
         [Column("isValid")]
         [StringLength(1)]
         [AllowNull]
-        public string? Vigente { get; set; } // Validación previa
+        public string? IsValid { get; set; } // Validación previa
+
+
+        public Socio(int idDx, string email, string nombre, string apellido, string? numeroDocumento, DateTime? fechaNacimiento, string? celular, string? activo, string? tarjetaIngreso, string? domicilio, string? domicilioConPiso, string? imagenUrl, string? sexo, string? vigente)
+        {
+            IdDx = idDx;
+            Email = email;
+            Nombre = nombre;
+            Apellido = apellido;
+            NumeroDocumento = numeroDocumento;
+            FechaNacimiento = fechaNacimiento;
+            Celular = celular;
+            IsActive = activo;
+            CardNumber = tarjetaIngreso;
+            Address = domicilio;
+            AddressWithFloor = domicilioConPiso;
+            ImageUrl = imagenUrl;
+            Gender = sexo;
+            IsValid = vigente;
+        }
+
+
+
     }
 }
