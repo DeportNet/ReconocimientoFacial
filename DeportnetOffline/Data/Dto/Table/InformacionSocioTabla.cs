13de87ee36057fdfefe341d07f5164babc26ad0b
facundoprocelli <128741375+facundoprocelli@users.noreply.github.com>
2025-04-21 12:38:57 -0300

    me falto esto

---
+++ DeportnetOffline/Data/Dto/Table/InformacionSocioTabla.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeportnetOffline.Data.Dto.Table
+{
+    public class InformacionSocioTabla
+    {
+
+        public string NombreYApellido {  get; set; }
+        public string NroTarjeta {  get; set; }
+        public string DNI { get; set; }
+        public string Email { get; set; }
+        public string Edad {  get; set; }
+        public string Celular { get; set; }
+        public string Direccion { get; set; }
+        public string Piso { get; set; }
+        public string Sexo {  get; set; }
+        public string Estado {  get; set; }
+    }
+}
